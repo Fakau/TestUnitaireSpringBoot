47d2101411b328c0ab0539fe6bb1921c0430b2eb
Laurent Kafka M <laurentkafka123@gmail.com>
2020-10-27 17:02:14 -0400

    add, 7 tests

--- src/main/java/com/engine/fakau/TestUnitaire/repository/BankAccountRepository.java
+++ src/main/java/com/engine/fakau/TestUnitaire/repository/BankAccountRepository.java
@@ -1,9 +1,22 @@
 package com.engine.fakau.TestUnitaire.repository;
 
-import com.engine.fakau.TestUnitaire.domain.BankAccoount;
+import com.engine.fakau.TestUnitaire.domain.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface BankAccountRepository extends JpaRepository<BankAccoount,Long> {
+public interface BankAccountRepository extends JpaRepository<BankAccount,Long> {
+    Optional<BankAccount> findOneByBankAccountNo(String bankAccountNo);
+
+    @Query("SELECT B FROM BankAccount B WHERE B.amount =(" +
+            "SELECT max(BS.amount) FROM BankAccount BS ) ")
+    List<BankAccount> findAllBankAccountWithMaxAmmout();
+
+    @Query("SELECT max(BS.amount) FROM BankAccount BS")
+    BigDecimal findMaxBankAccountAmmout();
 }

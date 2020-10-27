ae0f3c4a89c7b26f032dcc268175ec13a5ef5b3b
Laurent Kafka M <laurentkafka123@gmail.com>
2020-10-27 13:46:51 -0400

    first commit

---
+++ src/main/java/com/engine/fakau/TestUnitaire/TestUnitaireApplication.java
@@ -0,0 +1,13 @@
+package com.engine.fakau.TestUnitaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestUnitaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestUnitaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engine/fakau/TestUnitaire/TestUnitaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engine.fakau.TestUnitaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestUnitaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
